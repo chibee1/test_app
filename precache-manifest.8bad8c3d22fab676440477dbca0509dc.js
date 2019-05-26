e5d8f328527396402bdcbcfea7449f6a0292515e
chibee1 <cikuyama@gmail.com>
2019-05-26 18:43:51 -0400

    Updates

---
+++ precache-manifest.8bad8c3d22fab676440477dbca0509dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad3290970cc2b1807294e12e1dcb2dd",
+    "url": "/test_app/index.html"
+  },
+  {
+    "revision": "d3429ca229f8fd0ed4c1",
+    "url": "/test_app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test_app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d3429ca229f8fd0ed4c1",
+    "url": "/test_app/static/js/main.aeb29e17.chunk.js"
+  },
+  {
+    "revision": "577740571b468029c3bc",
+    "url": "/test_app/static/js/runtime~main.5b3a4ee9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
